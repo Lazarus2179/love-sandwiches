a30778fab079ff1c1d62b2a6098cf4bd33c359ba
Lazarus2179 <micael.lazaro@gmail.com>
2023-11-19 04:54:59 +0000

    Update run.py
    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service account import Credentials
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -8,12 +8,15 @@ SCOPE = [
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_sccopes(SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    Get sales figures input from the user
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separatedby commas.")
+    print("Exemple: 10,20,30,40,50,60")
 
-data = sales.get_all_values
-
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
